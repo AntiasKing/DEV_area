fef60c44f777bbb9c0e197cd146919fa02e72124
Antoine Briaux <antoine.briaux@epitech.eu>
2019-01-15 15:19:13 +0100

    [feat] l2 && l8 add morgan

--- server/src/index.js
+++ server/src/index.js
@@ -1,5 +1,10 @@
 const express = require('express');
+const morgan = require('morgan')
 
 const app = express();
 
 const port = 8080;
+
+app.use(morgan('dev'));
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
