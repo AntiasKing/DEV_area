50f80c7245b13b50d213a68eb1f738147cfc7268
Antoine Briaux <antoine.briaux@epitech.eu>
2019-02-13 14:55:42 +0100

    test

--- server/src/routes/webhook/facebook.js
+++ server/src/routes/webhook/facebook.js
@@ -1,9 +1,13 @@
-const verifyToken = "JeSuisTaMaman";
+const verifyToken = "Bonjour la mifa";
 
 module.exports = function (router, usersRef) {
     router.get('/webhook/facebook', function (req, res) {
         console.log(req.query['hub.mode']);
         console.log(req.query['hub.challenge']);
         console.log(req.query['hub.verify_token']);
+        if (req.query['hub.verify_token']) {
+            return res.status(200).send(req.query['hub.verify_token']);
+        }
+        return res.status(401).send();
     });
 }
