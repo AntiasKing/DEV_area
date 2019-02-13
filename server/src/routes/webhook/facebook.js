1c1b841741a77f9c7ed1795d4691d8bcf4ebd323
Antoine Briaux <antoine.briaux@epitech.eu>
2019-02-13 14:37:09 +0100

    test

--- server/src/routes/webhook/facebook.js
+++ server/src/routes/webhook/facebook.js
@@ -1,7 +1,7 @@
 const verifyToken = "JeSuisTaMaman";
 
 module.exports = function (router, usersRef) {
-    router.post('./webhook/facebook', function (req, res) {
-        console.log(req);
+    router.get('./webhook/facebook', function (req, res) {
+        console.log(req.query);
     });
 }
