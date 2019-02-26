51801c8a7e1c417335089083075f2dc99ac794d2
Antoine Briaux <antoine.briaux@epitech.eu>
2019-02-26 15:13:50 +0100

    twitch

--- server/src/routes/user/create.js
+++ server/src/routes/user/create.js
@@ -24,20 +24,20 @@ module.exports = function (router, usersRef) {
                 console.log(err);
                 return res.status(500).send(err);
             }
-            request.get('https://api.twitch.tv/helix/users', {
-                'auth': {
+            // request.get('https://api.twitch.tv/helix/users', {
+            //     'auth': {
 
-                    'bearer': body.access_token
-                }
-            }, function (err, r, body) {
-                if (err) {
-                    console.log(err);
-                    return res.status(500).send(err);
-                }
-                console.log("========================================");
-                console.log(body);
-                console.log("========================================");
-            })
+            //         'bearer': body.access_token
+            //     }
+            // }, function (err, r, body) {
+            //     if (err) {
+            //         console.log(err);
+            //         return res.status(500).send(err);
+            //     }
+            //     console.log("========================================");
+            //     console.log(body);
+            //     console.log("========================================");
+            // })
             return res.redirect('http://localhost:3000/' + '?access_token=' + body.access_token);
         })
     });
