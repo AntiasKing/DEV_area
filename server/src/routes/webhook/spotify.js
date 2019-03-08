f9f76207e9ef72b1c1a930004957419f0dc876fc
Pierre Narcisi <pierre.narcisi@epitech.eu>
2019-03-08 23:58:31 +0100

    adding spotify webhook

---
+++ server/src/routes/webhook/spotify.js
@@ -0,0 +1,9 @@
+const request = require('request');
+
+module.exports = function () {
+		function FollowPlaylist() {
+			let polling = setInterval(() => {
+				
+			}, 5000);
+		};
+};
---
+++ server/src/routes/webhook/twitchCreate.js
@@ -0,0 +1,46 @@
+const request = require('request');
+
+module.exports = {
+	TwitchFollows: function(twitchID, gain) {
+		let user;
+		if (gain === 0) {
+			user = 'to_id='+twitchID;
+		} else {
+			user = 'from_id='+twitchID;
+		}
+		request.post({
+			url: 'https://api.twitch.tv/helix/webhooks/hub',
+			headers: {
+				'Client-ID': 'gh2sbdqqplvq5qa89ze2h6e6zb4tur'
+			},
+			payload: {
+				'hub.callback': 'https://staging-area-epitech.herokuapp.com/webhooks/twitch/follows',
+				'hub.mode': 'subscribe',
+				'hub.topic': 'https://api.twitch.tv/helix/users/follows?first=1&'+user
+			}
+		}, function(err, response, body){
+			if (err) {
+				console.log(err);
+				return res.status(500).send(err);
+			}
+		});
+	},
+	streamFollows: function(twitchID) {
+		request.post({
+			url: 'https://api.twitch.tv/helix/webhooks/hub',
+			headers: {
+				'Client-ID': 'gh2sbdqqplvq5qa89ze2h6e6zb4tur'
+			},
+			payload: {
+				'hub.callback': 'https://staging-area-epitech.herokuapp.com/webhooks/twitch/stream',
+				'hub.mode': 'subscribe',
+				'hub.topic': 'https://api.twitch.tv/helix/streams?user_id='+twitchID
+			}
+		}, function(err, response, body){
+			if (err) {
+				console.log(err);
+				return res.status(500).send(err);
+			}
+		});
+	}
+}
