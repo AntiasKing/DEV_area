746a2dbb64373897682c3f5c945d43ee086f5ea6
Pierre Narcisi <pierre.narcisi@epitech.eu>
2019-03-09 15:56:44 +0100

    testing webhooks

--- server/src/routes/webhook/twitchCreate.js
+++ server/src/routes/webhook/twitchCreate.js
@@ -15,9 +15,12 @@ module.exports = {
 			headers: {
 				'Client-ID': 'gh2sbdqqplvq5qa89ze2h6e6zb4tur'
 			},
-			'hub.mode': 'subscribe',
-			'hub.callback': 'https://staging-area-epitech.herokuapp.com/webhooks/twitch/follows',
-			'hub.topic': 'https://api.twitch.tv/helix/users/follows?first=1&'+user
+			hub: {
+				'callback': 'https://staging-area-epitech.herokuapp.com/webhooks/twitch/follows',
+				'mode': 'subscribe',
+				'topic': 'https://api.twitch.tv/helix/users/follows?first=1&'+user
+				
+			}
 		}, function(err, response, body){
 			if (err) {
 				console.log(err);
