72066a88763f58abfcd67be9072730f68ceaa34b
Axel <axel.vandenabeele@epitech.eu>
2019-03-10 15:31:19 +0100

    [fix] : add weather applets

--- server/src/routes/actions/weather.js
+++ server/src/routes/actions/weather.js
@@ -7,15 +7,12 @@ module.exports = {
 	        var options = {
 	            method: 'GET',
 	            url: 'http://api.apixu.com/v1/forecast.json',
-	            qs: { key: '687f2f78d994457bb25130501190403', q: 'Montpellier', days: '2' }
+	            qs: { key: '7e7ac12f12194438bfd132440191003', q: 'Montpellier', days: '2' }
 	        };
 	        request(options, function (error, response, body) {
 	            if (error) throw new Error(error);
 	            let Val = JSON.parse(body);
-							console.log(Val)
-							console.log(Val.forecast)
 	            let res = Val.forecast.forecastday[1].day.condition.text;
-							console.log("debug")
 							const action = require("../applets/actions");
 	            if ((res.indexOf("rain") > -1 || res.indexOf("Rain") > -1) && id == 0) {
 								action.setServiceAction(applet, user, null)
@@ -24,12 +21,11 @@ module.exports = {
 							} else if ((res.indexOf("snow") > -1 || res.indexOf("Snow") > -1) && id == 2) {
 								action.setServiceAction(applet, user, null)
 							} else if ((res.indexOf("cloudy") > -1 || res.indexOf("Cloudy") > -1) && id == 3) {
-								console.log("okokok");
 								action.setServiceAction(applet, user, null)
 							}
 	        });
 	    this.getWeather(applet, user, id);
-		}, (applet.interval * 10000))
+		}, (applet.interval * 60000))
 	}
 
 }
