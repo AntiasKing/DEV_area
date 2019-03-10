68032a19f757e0768e99985ca881b59cd064db5e
Pierre Narcisi <pierre.narcisi@epitech.eu>
2019-03-10 23:26:16 +0100

    merge

--- server/src/routes/webhook/spotify.js
+++ server/src/routes/webhook/spotify.js
@@ -1,64 +1,62 @@
+const action = require('../applets/actions');
 const request = require('request');
 
 module.exports = {
-	handlePlaylist: function(oldPlaylist, newPlaylist, user) {
-		if (user.val().applets) {
-			user.val().applets.forEach(function(appletsnap) {
-				if (appletsnap.playlistID) {
-					if (oldPlaylist.id === appletsnap.playlistID) {
-						if (appletsnap.actionID === 0) {
-							if (oldPlaylist.tracks.length < newPlaylist.tracks.length) {
-								const action = require('../applets/actions');
-							}
-						}
-					}
+	FollowPlaylist: function(playlistID, spotifyID, interval, usersRef, access_token) {
+		let polling = setInterval(() => {
+			request.get({
+				url: 'https://api.spotify.com/v1/playlists/'+playlistID,
+				headers: {
+					'Authorization': 'Bearer '+access_token
 				}
-			});
-		}
-	},
-		FollowPlaylist: function(playlistID, spotifyID, interval, usersRef, access_token) {
-			let polling = setInterval(() => {
-				request.get({
-					url: 'https://api.spotify.com/v1/playlists/'+playlistID,
-					headers: {
-						'Authorization': 'Bearer '+access_token
-					}
-				}, function (err, response, body) {
-					if (err) {
-						console.log(err);
-						return res.status(500).send(err);
-					}
-					object = JSON.parse(body);
-					let present = 0;
-					usersRef.once('value')
-						.then(function (snapshot) {
-							snapshot.forEach(function (childSnapshot) {
-								if (childSnapshot.val().spotify) {
-									let spotify = childSnapshot.val().spotify;
-									if (spotify.id === spotifyID) {
-										if (!spotify.playlist) {
-											spotify.playlist = [];
-										}
-										spotify.playlist.forEach(function (playlist) {
-											if (playlist.id === object.id) {
-												present = 1;
-												if (playlist.snapshot_id !== object.snapshot_id) {
-													handlePlaylist(playlist, object, childSnapshot);
-													playlist = object;
-												}
-											}
-										})
-										if (present === 0) {
-											spotify.playlist.push(object);	
-										}
-										childSnapshot.child("spotify").ref.update(spotify);
+			}, function (err, response, body) {
+				if (err) {
+					console.log(err);
+					return res.status(500).send(err);
+				}
+				object = JSON.parse(body);
+				let present = 0;
+				usersRef.once('value')
+					.then(function (snapshot) {
+						snapshot.forEach(function (childSnapshot) {
+							if (childSnapshot.val().spotify) {
+								let spotify = childSnapshot.val().spotify;
+								if (spotify.id === spotifyID) {
+									if (!spotify.playlist) {
+										spotify.playlist = [];
 									}
+									spotify.playlist.forEach(function (playlist) {
+										if (playlist.id === object.id) {
+											present = 1;
+											if (playlist.snapshot_id !== object.snapshot_id) {
+												(playlist, object, childSnapshot);
+												if (childSnapshot.val().applets) {
+													childSnapshot.val().applets.forEach(function(appletsnap) {
+														if (appletsnap.playlistID) {
+															if (playlist.id === appletsnap.playlistID) {
+																if (appletsnap.actionID === 0) {
+																	if (playlist.tracks.length < object.tracks.length) {
+																		action.detectSpotify(object, appletsnap, childSnapshot);
+																	}
+																}
+															}
+														}
+													});
+												}
+												playlist = object;
+											}
+										}
+									})
+									if (present === 0) {
+										spotify.playlist.push(object);	
+									}
+									childSnapshot.child("spotify").ref.update(spotify);
 								}
-							})
+							}
 						})
-					}
-				)
-			}, interval*1000);
-		}
-
-	};
+					})
+				}
+			)
+		}, interval*1000);
+	}
+};
